bf30f23fb7a29f81ea14c9ebd85043b7ebfd223d
foksurek <foksureq@gmail.com>
2024-03-30 13:42:12 +0100

    removed unused code

--- Controllers/PlayerInfoController.cs
+++ Controllers/PlayerInfoController.cs
@@ -29,65 +29,6 @@ public class PlayerInfoController(
         return Ok(responseService.Ok(data));
     }
     
-    // [HttpGet("GetScores")]
-    // public async Task<IActionResult> GetScores(int id, int mode, string scope, int limit = 20, int offset = 0)
-    // {
-    //     if (scope != "best" && scope != "recent") return BadRequest(responseService.BadRequest(["Invalid scope parameter"]));
-    //     var best = scope == "best";
-    //     var scores = await scoresRepository.GetPlayerTop(id, mode, best, limit, offset);
-    //     var result = new List<object>();
-    //     //TODO: cache beatmaps
-    //     foreach (var score in scores)
-    //     {
-    //         var beatmap = await beatmapRepository.GetBeatmap(score.MapMd5);
-    //         if (beatmap == null) continue;
-    //         result.Add(new
-    //         {
-    //             id = score.Id,
-    //             score = score.Score,
-    //             pp = score.Pp,
-    //             acc = score.Acc,
-    //             max_combo = score.MaxCombo,
-    //             mods = score.Mods,
-    //             n300 = score.N300,
-    //             n100 = score.N100,
-    //             n50 = score.N50,
-    //             nmiss = score.NMiss,
-    //             ngeki = score.NGeki,
-    //             nkatu = score.NKatu,
-    //             grade = score.Grade,
-    //             status = score.Status,
-    //             mode = score.Mode,
-    //             play_time = score.PlayTime,
-    //             time_elapsed = score.TimeElapsed,
-    //             perfect = score.Perfect,
-    //             beatmap = new
-    //             {
-    //                 md5 = beatmap.Md5,
-    //                 id = beatmap.Id,
-    //                 set_id = beatmap.SetId,
-    //                 artist = beatmap.Artist,
-    //                 title = beatmap.Title,
-    //                 version = beatmap.Version,
-    //                 creator = beatmap.Creator,
-    //                 last_update = beatmap.LastUpdate,
-    //                 total_length = beatmap.TotalLength,
-    //                 max_combo = beatmap.MaxCombo,
-    //                 status = beatmap.Status,
-    //                 plays = beatmap.Plays,
-    //                 passes = beatmap.Passes,
-    //                 mode = beatmap.Mode,
-    //                 bpm = beatmap.Bpm,
-    //                 cs = beatmap.CircleSize,
-    //                 od = beatmap.OverallDifficulty,
-    //                 ar = beatmap.ApproachRate,
-    //                 hp = beatmap.HealthDrain,
-    //                 diff = beatmap.Difficulty,
-    //             }
-    //         });
-    //     }
-    //     return Ok(responseService.Ok(result));
-    // }
     [HttpGet("GetScores")]
     public async Task<IActionResult> GetScores(int id, int mode, string scope, int limit = 20, int offset = 0)
     {
@@ -101,8 +42,8 @@ public class PlayerInfoController(
         {
             id = tuple.Score.Id,
             score = tuple.Score.Score,
-            pp = tuple.Score.Pp,
-            acc = tuple.Score.Acc,
+            pp = Math.Round(tuple.Score.Pp, 2),
+            acc =  Math.Round(tuple.Score.Acc, 2),
             max_combo = tuple.Score.MaxCombo,
             mods = tuple.Score.Mods,
             n300 = tuple.Score.N300,
@@ -134,11 +75,11 @@ public class PlayerInfoController(
                 passes = tuple.Beatmap.Passes,
                 mode = tuple.Beatmap.Mode,
                 bpm = tuple.Beatmap.Bpm,
-                cs = tuple.Beatmap.CircleSize,
-                od = tuple.Beatmap.OverallDifficulty,
-                ar = tuple.Beatmap.ApproachRate,
-                hp = tuple.Beatmap.HealthDrain,
-                diff = tuple.Beatmap.Difficulty,
+                cs =  Math.Round(tuple.Beatmap.CircleSize, 2),
+                od =  Math.Round(tuple.Beatmap.OverallDifficulty, 2),
+                ar =  Math.Round(tuple.Beatmap.ApproachRate, 2),
+                hp =  Math.Round(tuple.Beatmap.HealthDrain, 2),
+                diff =  Math.Round(tuple.Beatmap.Difficulty, 2),
             }
         });
 
